4b9b853bb9c9ba4d71130fd0bdf093e96acabbef
Pradeep <venkata.pothineni@gmail.com>
2017-05-07 23:35:03 +0000

    renamed to generateNewUrl.js

---
+++ generateNewUrl.js
@@ -0,0 +1,44 @@
+module.exports = exports = {}
+var shortDb = require('./shortDb.js')
+var basePlusconvertedUrl
+exports.getNewUrl = function(baseUrl, callback) {
+    var x = "/" + exports.convertedUrl()
+    generateNewUrl(x)
+
+    function generateNewUrl(x) {
+
+        if (exports.check(x)) {
+            basePlusconvertedUrl = "https://latesthollerclone-curious2code.c9users.io" + x
+                //var basePlusconvertedUrl = "https://c9surl.herokuapp.com" + x
+            if (typeof(baseUrl) == 'string') {
+                shortDb.saveUrl(baseUrl, x)
+                callback(null, basePlusconvertedUrl)
+            }
+            else
+                callback("not a string")
+        }
+    }
+}
+exports.convertedUrl = function() {
+    return Math.random().toString(36).slice(-4)
+}
+
+exports.regenerate = function(x) {
+    x = "/" + exports.convertedUrl()
+    exports.getNewUrl.generateNewUrl(x)
+}
+
+exports.check = function(x) {
+    var y = []
+    shortDb.getUrl(x, function(err, data) {
+        if (err) throw err
+            //console.log("data is ", data)
+        y = data
+    })
+    if (y[0]) {
+        exports.regenerate(x)
+        return false
+    }
+    else return true
+
+}
