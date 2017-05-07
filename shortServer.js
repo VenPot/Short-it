fb2651dfc8d7556a0b9ddfe29840c597ddab530b
Pradeep <venkata.pothineni@gmail.com>
2017-05-07 23:36:25 +0000

    made it modular

--- shortServer.js
+++ shortServer.js
@@ -5,6 +5,7 @@ var path = require('path')
 var qs = require('querystring')
 var logger = require('morgan')
 var shortDb = require('./shortDb.js')
+var gnu = require('./generateNewUrl.js')
 var parsedBody = ''
 var filename
 var counter = 0
@@ -33,44 +34,10 @@ app.post('/sends', function(req, res) {
     req.on('end', function() {
         parsedBody = qs.parse(body)
         var baseUrl = parsedBody.clientUrl
-        var x = "/" + convertedUrl()
-        finish()
-
-        function finish() {
-
-            if (check(x)) {
-                var basePlusconvertedUrl = "https://latesthollerclone-curious2code.c9users.io" + x
-                    //var basePlusconvertedUrl = "https://c9surl.herokuapp.com" + x
-                shortDb.saveUrl(baseUrl, x)
-                res.end(basePlusconvertedUrl)
-            }
-        }
-
-        function convertedUrl() {
-            return Math.random().toString(36).slice(-4)
-        }
-
-        function regenerate() {
-            x = "/" + convertedUrl()
-            finish()
-        }
-
-        function check(x) {
-            var y = []
-            shortDb.getUrl(x, function(err, data) {
-                if (err) throw err
-                console.log("data is ", data)
-                y = data
-            })
-            if (y[0]) {
-                regenerate()
-                return false
-            }
-            else return true
-
-        }
-
-
+        gnu.getNewUrl(baseUrl, function(err, data) {
+            if (err) console.log(err)
+            else res.end(data)
+        })
     })
 
 })
