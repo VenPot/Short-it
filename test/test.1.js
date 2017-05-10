cd4033a4f004d69af38214d81be64128d8331665
Pradeep <venkata.pothineni@gmail.com>
2017-05-10 01:03:05 +0000

    moved test files to test directory

--- test/test.1.js
+++ test/test.1.js
@@ -1,28 +1,13 @@
 var chai = require('chai')
 var chaihttp = require('chai-http')
 var assert = require('chai').assert;
-var gnu = require('./generateNewUrl.js')
-var shortDb = require('./shortDb.js')
+var gnu = require('../generateNewUrl.js')
+var shortDb = require('../shortDb.js')
 chai.use(chaihttp)
 
 
-describe('#Generate New URL', function() {
+describe('#database calls', function() {
 
-  it('check if input should be a string', function(done) {
-    gnu.getNewUrl('hello', 'testhost', function(err, data) {
-      if (err) console.log(err)
-      assert.typeOf(data, 'string', 'yes it is a string ')
-      done()
-    })
-  })
-
-  it('if input is number -should throw "not a string"  error', function(done) {
-    gnu.getNewUrl(1, "testhost", function(err, data) {
-      //console.log("err.msg is ", err.msg)
-      assert.equal(err, 'not a string')
-      done()
-    })
-  })
 
   it('database call should resolve to some data when passing existing handle', function(done) {
 
